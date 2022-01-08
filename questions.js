892ab201ca240823bfa1cce1c4973dfac6a25092
Coder102 <73834918+adwaithokam193@users.noreply.github.com>
2022-01-08 11:44:42 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,128 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which is not a coding language?",
+    answer: "BANANA",
+    options: [
+      "TextScript",
+      "GO",
+      "SQL",
+      "BANANA"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Python is ___ programming language?",
+    answer: "High level language",
+    options: [
+      "High level language",
+      "Mid level language",
+      "Low level Language",
+      "Very High level language"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Whatsapp corcurrent model is implemented using __ programming language.?",
+    answer: "Erlang",
+    options: [
+      "Java",
+      "Node.js",
+      "Erlang",
+      "C"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6 ,
+    question: "Which is not an IDE?",
+    answer: "C#",
+    options: [
+      "Anaconda",
+      "Pycharm",
+      "C#",
+      "Spyder"
+    ]
+  },
+  {
+    numb: 7,
+    question: "How many windows does Python language use?",
+    answer: "Two",
+    options: [
+      "Four",
+      "Three",
+      "Two",
+      "One"
+    ]
+  },
+  {
+    numb: 8,
+    question: "WHich of these is a programming language?",
+    answer: "Scratch",
+    options: [
+      "Bite",
+      "Gnaw",
+      "Scratch",
+      "Itch"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What are people who write computer code called?",
+    answer: "Programmer",
+    options: [
+      "Programmer",
+      "Professors",
+      "Manufractors",
+      "Cryptographers"
+    ]
+  },
+  {
+    numb: 10,
+    question: "WHich of these does NOT run using a computer program?",
+    answer: "Bicycle",
+    options: [
+      "Rocket",
+      "Car",
+      "Bicyle",
+      "Train"
+    ]
+  },
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
